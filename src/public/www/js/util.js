1739650ff414e5510bf12720a6a625b21293c66b
Aytixel <aytixel@gmail.com>
2022-02-18 19:33:49 +0100

    Initial page structure

---
+++ src/public/www/js/util.js
@@ -0,0 +1,32 @@
+export function throttle(callback, delay) {
+  let last, timer;
+
+  return function () {
+    let context = this, now = +new Date(), args = arguments;
+
+    if (last && now < last + delay) {
+      clearTimeout(timer);
+
+      timer = setTimeout(() => {
+        last = now;
+        callback.apply(context, args);
+      }, delay);
+    } else {
+      last = now;
+      callback.apply(context, args);
+    }
+  };
+}
+
+export function debounce(callback, delay) {
+  let timer;
+
+  return function () {
+    let args = arguments, context = this;
+
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+      callback.apply(context, args);
+    }, delay);
+  };
+}
